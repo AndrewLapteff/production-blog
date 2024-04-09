a5004ddf9bbf44d9bb16493b9026fd62bd82a0ee
Andrew Lapteff <this.is.amigogo@gmail.com>
2024-04-09 21:27:20 +0300

    Fix bugs

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,3 +1,3 @@
-export { Article } from './ui/article/Article'
+export { Article } from 'entities/Article/ui/article-component/Article'
 export type * from './model/types/article'
 export * from './model/slice/articleSlice'
